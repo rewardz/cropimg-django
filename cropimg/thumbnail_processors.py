a8290db1119af4bce4d94b4157360a64de0c4367
Ramast Magdy <ramast.com@gmail.com>
2016-07-21 18:19:22 +0800

    add ci_ prefix to easythumbnail processor to avoid conflict with django-image-cropping app

--- cropimg/thumbnail_processors.py
+++ cropimg/thumbnail_processors.py
@@ -4,20 +4,20 @@ except ImportError:
     import Image
 
 
-def crop_box(im, size, box=False, **kwargs):
+def crop_ci_box(im, size, ci_box=False, **kwargs):
     """
     Crop image based on very specific pixel values (x,y,width,height)
 
-    box
+    ci_box
         Crop the source image to exactly match the requested box
     """
-    if not box:
+    if not ci_box:
         return im
 
-    assert isinstance(box, basestring)
-    if box.count(",") != 3:
-        raise ValueError("box must contain exactly 4 values x,y,width,height")
-    x, y, width, height = [int(i) for i in box.split(",")]
+    assert isinstance(ci_box, basestring)
+    if ci_box.count(",") != 3:
+        raise ValueError("ci_box must contain exactly 4 values x,y,width,height")
+    x, y, width, height = [int(i) for i in ci_box.split(",")]
     # Handle one-dimensional targets.
     im = im.convert("RGBA").crop((x, y, x + width, y + height))
     return im
