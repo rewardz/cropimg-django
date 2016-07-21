fb3004c4d2a4d60947b60a080f3e27c33730fe43
Ramast Magdy <ramast.com@gmail.com>
2016-07-21 18:33:24 +0800

    Include all package data

--- setup.py
+++ setup.py
@@ -91,9 +91,7 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-       'cropimg': ['*.html', '*.js', '*.css', '*.png'],
-    },
+    include_package_data = True
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
