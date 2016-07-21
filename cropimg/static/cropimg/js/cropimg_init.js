ee41d0a63e78dbce71948d9813fb39fce2d44392
Ramast Magdy <ramast.com@gmail.com>
2016-07-21 17:08:44 +0800

    Fixed bugs that happen when more than one cropimg fields are present for same image

--- cropimg/static/cropimg/js/cropimg_init.js
+++ cropimg/static/cropimg/js/cropimg_init.js
@@ -1,19 +1,27 @@
 void(function() {
-    function on_file_change() {
+
+    function on_file_change(thumbnail_data) {
         if (this.files && this.files[0]) {
             var reader = new FileReader();
-            var thumbnail_data = $(this).data("thumbnail-data");
             var thumbnail = $(thumbnail_data.thumbnail_image_id);
             thumbnail.attr('src', '');
             $("#" + thumbnail_data.my_id).val("");
-            thumbnail.data("cropimg").reset();
+            if (thumbnail.data("cropimg")) {
+                thumbnail.data("cropimg").reset();
+            }
             reader.onload = function (e) {
                 thumbnail = $(thumbnail_data.thumbnail_image_id);
                 thumbnail.attr('src', e.target.result);
-                thumbnail.cropimg(thumbnail_data.cropimg_args);
             }.bind(this);
 
             reader.readAsDataURL(this.files[0]);
+        }
+    }
+
+    function on_thumbnail_load(thumbnail_data) {
+        var thumbnail = $(this);
+        if (thumbnail.attr("src")) {
+            thumbnail.cropimg(thumbnail_data.cropimg_args);
         }
     }
 
@@ -48,18 +56,16 @@ void(function() {
         var $field = $("#" + data.my_id);
         var thumbnail_image = $(data.thumbnail_image_id);
         //If the field is not empty, update the cropimg to match its values
-        if (thumbnail_image.attr("src")) {
-            var x, y, w, h;
-            var cropimg = thumbnail_image.data("cropimg");
-            if ($field.val()) {
-                var vallist = $field.val().split(",");
-                x = parseInt(vallist[0]);
-                y = parseInt(vallist[1]);
-                w = parseInt(vallist[2]);
-                setTimeout(setImgCropData, 500, cropimg, x, y, w);
-            } else { // No data set, default to whole image
-                setTimeout(setImgCropData, 500, cropimg, 0, 0);
-            }
+        var x, y, w, h;
+        var cropimg = thumbnail_image.data("cropimg");
+        if ($field.val()) {
+            var vallist = $field.val().split(",");
+            x = parseInt(vallist[0]);
+            y = parseInt(vallist[1]);
+            w = parseInt(vallist[2]);
+            setTimeout(setImgCropData, 700, cropimg, x, y, w);
+        } else { // No data set, default to whole image
+            setTimeout(setImgCropData, 700, cropimg, 0, 0);
         }
     }
 
@@ -69,24 +75,26 @@ void(function() {
             $field = $(my_field);
             var thumb_size = $field.attr("data-thumb-size").split(",").map(function(x) {return parseInt(x)});
             var data = {};
+            data.my_name = $field.attr("data-thumb-field");
+            data.my_id = $field.attr("id");
             data.cropimg_args = {
                 resultWidth: thumb_size[0], resultHeight: thumb_size[1],
+                displayFixingPositionsButtons: thumb_size[0] > 100,
+                displayFixingSizeButtons: thumb_size[0] > 100,
+                maxContainerWidth: 400, inputPrefix: "ci-" + data.my_id,
                 onChange: onCroppingSelectionChanged.bind(data),
                 onInit: onImgCropInit.bind(data)
             };
-            data.my_name = $field.attr("data-thumb-field");
-            data.my_id = $field.attr("id");
             data.image_field_id = "#" + data.my_id.replace(data.my_name, $field.attr("data-image-field"));
             var image_field = $(data.image_field_id);
             var image_value = image_field.attr("data-value");
             data.thumbnail_image_id = "#" + data.my_id + "-img";
             var thumbnail_image = $(data.thumbnail_image_id);
-            image_field.data("thumbnail-data", data);
-            image_field.change(on_file_change);
+            $field.data("thumbnail-data", data);
+            image_field.change($.proxy(on_file_change, image_field[0], data));
+            thumbnail_image.load($.proxy(on_thumbnail_load, thumbnail_image[0], data));
             thumbnail_image.attr("src", image_value);
-            thumbnail_image.cropimg(data.cropimg_args);
         });
     });
 }());
-
 
