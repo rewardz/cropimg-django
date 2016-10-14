baa922e6fd1ade236c3f1a7d43d976223280272b
Ramast Magdy <ramast.com@gmail.com>
2016-10-14 12:18:31 +0800

    Handle uninitalized imgcrop value

--- cropimg/thumbnail_processors.py
+++ cropimg/thumbnail_processors.py
@@ -15,9 +15,20 @@ def crop_box(im, size, ci_box=False, **kwargs):
         return im
 
     assert isinstance(ci_box, basestring)
-    if ci_box.count(",") != 3:
-        raise ValueError("ci_box must contain exactly 4 values x,y,width,height")
-    x, y, width, height = [int(i) for i in ci_box.split(",")]
+    try:
+        x, y, width, height = [int(i) for i in ci_box.split(",")]
+    except ValueError as e:
+        # try to guess why the exception happened
+        values = ci_box.split(",")
+        if len(values) != 4:
+            raise ValueError("crop_box must contain exactly 4 values x,y,width,height")
+        raise ValueError("crop_box processor got the following error"
+                         "when processing the value %s\n%s" % (ci_box, e.message))
+
+    if width <= 0 or height <= 0:
+        # That's basically an unintialized value
+        return im
+
     # Handle one-dimensional targets.
     im = im.convert("RGBA").crop((x, y, x + width, y + height))
     return im
